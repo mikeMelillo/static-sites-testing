02e6be6bc351240d854427af155a8ba8ddafd3ff
mikeMelillo <mmelillo@albireoenergy.com>
2025-09-22 23:45:40 -0400

    fix ups for correct directory and link generation

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,74 +1,53 @@
-from textnode import TextNode, TextType
-
 class HTMLNode:
-  def __init__(self, tag=None, value=None, children:list=None, props:dict=None):
-    self.tag = tag
-    self.value = value
-    self.children = children or []
-    self.props = props or {}
-  
-  def to_html(self):
-    raise NotImplementedError
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
 
-  def props_to_html(self):
-    res = ""
-    for key,value in self.props:
-      res += f' {key}="{value}"'
-    return res
-  
-  def __eq__(self, other):
-    return self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
 
-  def __repr__(self) -> str:
-    return f"Tag: {self.tag}, Value: {self.value}, Children: {self.children}, Props: {self.props}"
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
 
 class LeafNode(HTMLNode):
-  def __init__(self, tag=None, value=None, props:dict=None):
-    super().__init__(tag, value, [], props)
-  
-  def to_html(self):
-    if self.value == None:
-      raise ValueError("Node has no value set")
-    if self.tag == None:
-      return str(self.value)
-    props_str = f''
-    if self.props != None:
-      for key,value in self.props.items():
-        props_str += f' {key}="{value}"'
-    output = f'<{self.tag}{props_str}>{str(self.value)}</{self.tag}>'
-    return output
-  
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
 class ParentNode(HTMLNode):
-  def __init__(self, tag, children, props:dict=None):
-    super().__init__(tag, None, children=children, props=props)
-  
-  def to_html(self):
-    if self.tag == None:
-      raise ValueError("No tag set")
-    if self.children == None or len(self.children) == 0:
-      raise ValueError("No children defined for parent node")
-    props_str = f''
-    children_str = f''
-    for child in self.children:
-      children_str += child.to_html()
-    if self.props != None:
-      for key,value in self.props.items():
-        props_str += f' {key}="{value}"'
-    output = f'<{self.tag}{props_str}>{children_str}</{self.tag}>'
-    return output
-    
-def text_node_to_html_node(text_node:TextNode):
-  if text_node.text_type == TextType.TEXT:
-    return LeafNode(value=text_node.text)
-  elif text_node.text_type == TextType.BOLD:
-    return LeafNode("b", text_node.text)
-  elif text_node.text_type == TextType.ITALIC:
-    return LeafNode("i", text_node.text)
-  elif text_node.text_type == TextType.CODE:
-    return LeafNode("code", text_node.text)
-  elif text_node.text_type == TextType.LINK:
-    return LeafNode("a", text_node.text, {"href": text_node.url})
-  elif text_node.text_type == TextType.IMAGE:
-    return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-  else:
-    return ValueError("No valid text type for html conversion found")
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/main.py
+++ src/main.py
@@ -1,74 +1,32 @@
-from textnode import TextNode,TextType
-from htmlnode import HTMLNode, LeafNode, ParentNode
-from extract_title import *
 import os
+import shutil
 import sys
-from shutil import copy, rmtree
+
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
+
 
 def main():
-  try:
-    basepath = sys.argv[1]
-  except Exception:
-    basepath = ""
-  copy_contents(f"{basepath}static", f"{basepath}docs")
-  generate_pages_recursive(f"{basepath}content", "template.html", f"{basepath}docs")
-  
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-  source_contents = os.listdir(dir_path_content)
-  print(f"These contents: {source_contents}")
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
 
-  for item in source_contents:
-    print(f"Analyzing: {item}")
-    source_path = os.path.join(dir_path_content, item)
-    dest_path = os.path.join(dest_dir_path, item)
-    print(f"Source: {source_path}, Dest: {dest_path}")
-    if os.path.isdir(source_path) and not os.path.isdir(dest_path):
-      os.mkdir(dest_path)
-      print("Recursively walking tree..")
-      generate_pages_recursive(source_path, template_path, dest_path)
-    elif os.path.isfile(source_path):
-      print("Generating page..")
-      generate_page(source_path, template_path, dest_path)
-  pass
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
-def iter_dir(source_path, dest_path, new_dir):
-  new_source_path = os.path.join(source_path, new_dir)
-  new_dest_path = os.path.join(dest_path, new_dir)
-  print(f"New source: {new_source_path}, New dest: {new_dest_path}")
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
-  if os.path.isfile(new_source_path):
-    copy(new_source_path, new_dest_path)
-  elif not os.path.exists(new_dest_path):
-    os.mkdir(new_dest_path)
-  else:
-    next_dirs = os.listdir(new_source_path)
-    for dir in next_dirs:
-      iter_dir(new_source_path, new_dest_path, dir)
-  return None
-
-def copy_contents(source, dest):
-  root = True
-  if root:
-    rmtree(dest)
-    os.mkdir(dest)
-    root = False
-  
-  source_dir = os.listdir(source)
-
-  print(f"Starting out: {source_dir}")
-
-  for item in source_dir:
-    print(f"Working on {item}")
-    source_path = os.path.join(source, item)
-    dest_path = os.path.join(dest, item)
-    if os.path.isdir(source_path) and not os.path.exists(dest_path):
-      os.mkdir(dest_path)
-    if os.path.isfile(source_path):
-      copy(source_path, dest_path)
-    else:
-      new_dirs = os.listdir(source_path)
-      for new_dir in new_dirs:
-        iter_dir(source_path, dest_path, new_dir)
 
 main()
